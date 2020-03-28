ea5e276a65283692f0f654c72a4ed742920046fd
takiyoto <pedromiguelmvs@gmail.com>
2020-03-28 15:16:51 -0300

    Pasta com as funcionalidades em NodeJS

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+// Importando módulos
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
+
+// Iniciando o app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Iniciando a DB
+mongoose.connect('mongodb://localhost:27017/nodeapi',
+{ useNewUrlParser: true, useUnifiedTopology: true });
+
+requireDir('./src/models');
+
+// Rotas
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
